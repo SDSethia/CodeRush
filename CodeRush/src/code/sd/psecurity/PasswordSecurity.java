6ff2f6beeb12729004c5059c4ddc408627dd9024
SDSethia <snehadeepsethia@gmail.com>
2013-09-26 16:18:15 -0500

    Added author

--- CodeRush/src/code/sd/psecurity/PasswordSecurity.java
+++ CodeRush/src/code/sd/psecurity/PasswordSecurity.java
@@ -4,6 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 
+ * @author Snehadeep Sethia
+ * 
+ */
 public class PasswordSecurity {
 
 	public static void main(final String[] args) throws NumberFormatException,
